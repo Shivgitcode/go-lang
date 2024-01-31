491d86c273910839a8cfa901b41d24e34dfaf2b1
shivansh <shivneeraj2004@gmail.com>
2024-01-31 17:29:37 +0530

    done with user inputs , struct pointers and receiver functions in structs

--- structs/bill.go
+++ structs/bill.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type bill struct{
 	name string 
 	items map[string]float64
@@ -18,4 +20,42 @@ func newBill(name string) bill{
 
 	return b;
 
+}
+
+// format the bill 
+
+func (b *bill) format() string{
+	fs :="bill breakdown :\n"
+	var total float64=0
+
+	//list items;
+	for k,v:=range b.items{
+		fs +=fmt.Sprintf("%-25v ...$%v\n",k+":",v) // -25 signifies that the key takes a 25 character long value but the key is only 4 characters long so it will fill rest of the space with empty spaces and also -25 signifies that we have to give space to the right and 25 signifies space to the left
+		total+=v;
+	}
+
+	// add tip
+
+	fs +=fmt.Sprintf("%-25v ...$%0.2f\n","tip:",b.tip)
+
+	// total 
+
+	fs +=fmt.Sprintf("%-25v ...$%0.2f","total:",total+b.tip)
+
+
+	return fs
+
+}
+
+// update tip
+
+func (b *bill) updateTip(tip float64){
+	// (*b).tip=tip we can do this but 
+	b.tip=tip //when we pass pointer struct go automatically dereference the pointer values
+}
+
+//add an item to the bill
+
+func(b *bill) addItem(name string,price float64){
+	b.items[name]=price
 }
--- structs/main.go
+++ structs/main.go
@@ -1,10 +1,61 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getInput(prompt string,r *bufio.Reader)(string,error){
+	fmt.Print(prompt)
+	input,err:=r.ReadString('\n')
+
+	return strings.TrimSpace(input),err
+
+}
+
+func createBill() bill{
+	reader:=bufio.NewReader(os.Stdin)
+
+	// fmt.Print("Create a new bill name: ")
+	// name,_:=reader.ReadString('\n')
+	// name=strings.TrimSpace(name)
+
+	name,_:=getInput("Create a new bill name: ",reader)
+
+	b:=newBill(name)
+	fmt.Println("Created the bill - ",b.name)
+
+	return b
+}
+
+func promptOptons(b bill){
+	reader:=bufio.NewReader(os.Stdin)
+
+	// name,_:=getInput("Create a new bill name: ",reader)
+	opt,_:=getInput("Choose option (a-add item,s-save bill,t-add tip): ",reader)
+	fmt.Println(opt)
+
+
+}
 
 
 func main(){
-	 mybill:=newBill("mario's bill")
-	 fmt.Println(mybill)
+	//  mybill:=newBill("mario's bill")
+	 
+	// mybill.addItem("onion soup",4.50)
+	// mybill.addItem("veg pie",8.95)
+	// mybill.addItem("double latte",3.22)
+	//  mybill.updateTip(10)
+
+
+	//  fmt.Println(mybill.format())
+
+	mybill :=createBill()
+
+	fmt.Println(mybill)
+
+
 
 }
